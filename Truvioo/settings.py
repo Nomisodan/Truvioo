f695b690ffc479b8988b252f9214c84798a71b47
Nomisodan <124398186+Nomisodan@users.noreply.github.com>
2026-04-22 23:09:20 -0600

    add Cloudflare Stream integration and environment variables

--- Truvioo/settings.py
+++ Truvioo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-di8t^pl*7_y4gw31_xd5v2qizgo2nlss31v3)8q-k)x!1%v9cq'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
